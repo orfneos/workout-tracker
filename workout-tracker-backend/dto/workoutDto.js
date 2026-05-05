e3acb276af6807cdec6a92421e562bfe58a28edc
orfneos <orfneos@yahoo.gr>
2026-05-05 23:56:21 +0300

    refactor: remove user field from workoutDto

--- workout-tracker-backend/dto/workoutDto.js
+++ workout-tracker-backend/dto/workoutDto.js
@@ -2,7 +2,6 @@ exports.toDto = (workout) => {
   if (!workout) return null;
   return {
     _id: workout._id,
-    user: workout.user,
     date: workout.date,
     exercises: workout.exercises,
   };
